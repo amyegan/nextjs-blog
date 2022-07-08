ad7c6145da569f5940137a503abf47ea3c9e4275
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-07-07 20:22:38 -0400

    Add footer

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -26,9 +26,9 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        <a href="/" className={styles.siteName}>
+        <Link href="/" className={styles.siteName}>
           Site Name
-        </a>
+        </Link>
         <nav className={styles.bar}>
           <ul>
             <li>
@@ -43,12 +43,53 @@ export default function Layout({ children, home }) {
         </nav>
       </header>
 
-      <main className={styles.main}>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
+      <main className={styles.main}>
+        {children}
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/blog">← Back to blog list</Link>
+          </div>
+        )}
+      </main>
+
+      <footer className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <Link href="https://github.com/amyegan">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="var(--text)"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className={styles.feather}
+            >
+              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+            </svg>{" "}
+            GitHub
+          </Link>
+          <Link href="https://twitter.com/AmyAEgan">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className={styles.feather}
+            >
+              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+            </svg>{" "}
+            Twitter
+          </Link>
         </div>
-      )}
-    </>
+      </footer>
+    </div>
   );
 }
