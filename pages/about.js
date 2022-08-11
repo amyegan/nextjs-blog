26e071cbcac6a4a3cd0a04b5d6b8b3f22a64ba96
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-08-11 16:07:51 -0400

    change about

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,23 @@
-export default function About() {
-  console.log("process.env.VERCEL_URL", process.env.VERCEL_URL);
+export async function getStaticProps() {
+  return {
+    props: {
+      ...props,
+      staticUrl: process.env.VERCEL_URL,
+    },
+  };
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      ...props,
+      serverUrl: process.env.VERCEL_URL,
+    },
+  };
+}
+
+export default function About({ serverUrl, staticUrl }) {
+  console.log("staticUrl", staticUrl);
+  console.log("serverUrl", serverUrl);
   return <h1>About</h1>;
 }
