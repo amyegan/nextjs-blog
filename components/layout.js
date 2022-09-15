52b05a2f56eff02fa1eacfa256064aa1261c6807
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-09-15 11:54:29 -0400

    bee

--- components/layout.js
+++ components/layout.js
@@ -34,6 +34,7 @@ export default function Layout({ children, home, post }) {
           media="(prefers-color-scheme: light)"
           content="#f1f0ec"
         />
+        <script async data-api="/_hive" src="/bee.js"></script>
       </Head>
       <meta name="robots" content="all" />
       <header
